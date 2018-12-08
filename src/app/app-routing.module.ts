3bc918adad1c14c5a149cbde06d4ee917a292c37
Nikolay Sinuk <silverwolfkj2015@gmail.com>
2018-12-09 00:48:06 +0300

    Adding routing to product-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductComponent } from './UI/product/product.component';
 import { ProductDetailsComponent } from './UI/product/product-details/product-details.component';
+import { ProductListComponent } from './UI/product/product-list/product-list.component';
 
 const routes: Routes = [
   { path: '', component: AddProductComponent },
   { path: 'auth', component: AuthComponent },
   { path: 'product-details/:id', component: ProductDetailsComponent },
-  { path: 'add-product', component: AddProductComponent }
+  { path: 'add-product', component: AddProductComponent },
+  { path: 'products', component: ProductListComponent }
 ];
 
 @NgModule({
