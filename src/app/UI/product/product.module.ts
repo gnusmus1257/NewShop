e364cecbfe21ac2f8bd610fded27d4c1b4861882
Nikolay Sinuk <silverwolfkj2015@gmail.com>
2018-12-09 00:47:49 +0300

    Created product-list component with markup and css. Adding to product module link (declaration). Adding to product css style.

--- src/app/UI/product/product.module.ts
+++ src/app/UI/product/product.module.ts
@@ -1,6 +1,7 @@
 import { MatIconModule } from '@angular/material/icon';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
@@ -15,7 +16,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   declarations: [
     ProductComponent,
     ProductDetailsComponent,
-    AddProductComponent
+    AddProductComponent,
+    ProductListComponent
   ],
   imports: [
     CommonModule,
