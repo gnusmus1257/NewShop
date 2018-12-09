382dbe9af8a4bfb434d3d250fad294f3f94db711
Nikolay Sinuk <silverwolfkj2015@gmail.com>
2018-12-09 20:22:44 +0300

    algolia

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,6 +11,12 @@ export const environment = {
     projectId: 'shop-b7ac7',
     storageBucket: 'shop-b7ac7.appspot.com',
     messagingSenderId: '46182963584'
+  },
+  algolia: {
+  	appId: 'GN8EKPWZQ1',
+  	apiKey: 'efbe87c7d034c0bc1948ade6ca97bb64',
+    indexName: 'getstarted_actors',
+    urlSync: false
   }
 };
 
